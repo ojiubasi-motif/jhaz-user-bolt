936c71bb94cf18877a2f863ea79d18ba83b002c4
cryptedoji <ojiinnocent@gmail.com>
2026-06-11 22:27:29 +0100

    style: reposition and scale navbar logo to fit layout perfectly

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -85,11 +85,11 @@ export default function Navbar() {
       <div className="section-container">
         <div className="flex items-center justify-between h-16 sm:h-20">
           {/* Logo */}
-          <Link to="/" className="relative w-20 sm:w-24 h-16 sm:h-20 flex items-center group">
+          <Link to="/" className="flex items-center h-full group">
             <img 
               src={logo} 
               alt="Jhaz-imprints Logo" 
-              className="absolute top-2 sm:top-3 left-0 h-20 w-20 sm:h-24 sm:w-24 object-contain z-50 transition-all duration-300 transform-gpu will-change-transform [backface-visibility:hidden] group-hover:scale-105" 
+              className="h-12 w-12 sm:h-16 sm:w-16 object-contain transition-all duration-300 transform-gpu will-change-transform [backface-visibility:hidden] group-hover:scale-105" 
             />
           </Link>
 
